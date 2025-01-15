621da4722dfdb2b8e610656c7bfe4708f1b7b582
Reh1t <rehantariq@gmail.com>
2025-01-15 17:31:54 +0500

    update About page start date, change GitHub link, and add new tech stack icons

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -104,15 +104,18 @@ const techStacks = [
   { icon: "html.svg", language: "HTML" },
   { icon: "css.svg", language: "CSS" },
   { icon: "javascript.svg", language: "JavaScript" },
+  { icon: "typescript.svg", language: "Typescript" },
   { icon: "tailwind.svg", language: "Tailwind CSS" },
-  { icon: "reactjs.svg", language: "ReactJS" },
-  { icon: "vite.svg", language: "Vite" },
-  { icon: "nodejs.svg", language: "Node JS" },
   { icon: "bootstrap.svg", language: "Bootstrap" },
-  { icon: "firebase.svg", language: "Firebase" },
   { icon: "MUI.svg", language: "Material UI" },
+  { icon: "reactjs.svg", language: "ReactJS" },
+  { icon: "nodejs.svg", language: "Node JS" },
+  { icon: "vite.svg", language: "Vite" },
+  { icon: "firebase.svg", language: "Firebase" },
   { icon: "vercel.svg", language: "Vercel" },
-  { icon: "SweetAlert.svg", language: "SweetAlert2" },
+  { icon: "nextjs.svg", language: "Next.js" },
+  { icon: "python.svg", language: "Python" },
+  { icon: "Flask.svg", language: "Flask" },
 ];
 
 export default function FullWidthTabs() {
